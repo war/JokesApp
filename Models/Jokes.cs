c7c8653655224280dd8151f43627329feaf4449f
war <crypto22h@gmail.com>
2023-06-07 18:16:13 +0100

    Add Jokes model

---
+++ Models/Jokes.cs
@@ -0,0 +1,14 @@
+﻿namespace JokesApp.Models
+{
+    public class Jokes
+    {
+        public int ID { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        public Jokes()
+        {
+            
+        }
+    }
+}
